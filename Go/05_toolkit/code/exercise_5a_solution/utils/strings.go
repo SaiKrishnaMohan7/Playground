339e9a73f142da1e509d31aa042899fcfdefa738
Sai Krishna Mohan <saikrishnamohan@ibm.com>
2020-10-08 19:51:10 -0400

    Remove solutions add slides

--- Go/05_toolkit/code/exercise_5a_solution/utils/strings.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-import "strings"
-
-// MakeExcited transforms a sentence to all caps with an exclamation point
-func MakeExcited(sentence string) string {
-	return strings.ToUpper(sentence) + "!"
-}
--- Go/main.go
+++ Go/main.go
@@ -1,7 +1,13 @@
-package main
+package main // Package name, every go prog needs a package main
 
-import "fmt"
+import "fmt" // lib imports
 
+// Main entrypoint of the program; There can be only one main fucntion in a dir
+// Only one main function per package main
 func main() {
+	arg1 := "Shtah"
+
 	fmt.Println("Hello Front End Masters!")
+	fmt.Print("Hello Front End Masters!")              // Print vs Println :: no new line vs new line
+	fmt.Printf("Hello Front End Masters is %s!", arg1) // String formatting, %s, %c, %d
 }
