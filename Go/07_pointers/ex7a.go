8fedce49ab24a089dcdee9a8242166097f09ef99
Sai Krishna Mohan <saikrishnamohan@ibm.com>
2021-01-04 09:32:00 -0500

    commit

---
+++ Go/07_pointers/ex7a.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func updateEmail(user User, email string) string {
+	user.Email = email
+
+	return email
+}
+
+// User struct tries things
+type User struct {
+	ID                         int
+	FirstName, LastName, Email string
+}
+
+func main() {
+	user := User{ID: 1, FirstName: "Thitla", Email: "hello@challo.com"}
+	fmt.Println("User: ", user)
+	updateEmail(user, "challo@aao.com")
+	fmt.Println("User")
+}
