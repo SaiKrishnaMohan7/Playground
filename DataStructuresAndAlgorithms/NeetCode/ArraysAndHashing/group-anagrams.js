4bca0b865b237bcddcf9de607eb4d0a52c935e24
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2022-04-08 05:56:31 -0400

    Add grouping anagrams

---
+++ DataStructuresAndAlgorithms/NeetCode/ArraysAndHashing/group-anagrams.js
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/group-anagrams/
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+// Time: O(m * nlogn) n - str len, m - array len; Space: O(m + n)
+var groupAnagrams = function(strs) {
+  const map = {};
+
+  for (const str of strs) { // Note: for...in (loops over properties) should not be used to iterate over an Array where the index order is important.
+    let sortedStr = str.split('').sort().join('');
+    if (!map[sortedStr]) {
+      map[sortedStr] = [];
+    }
+    map[sortedStr].push(str);
+  }
+
+  const groupedAnagrams = Object.values(map);
+  return groupedAnagrams;
+};
+
+groupAnagrams(["eat","tea","tan","ate","nat","bat"]);
