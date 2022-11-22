414235275e9d7a7e62d37d54dd43d7a40904e555
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2022-11-21 22:33:16 -0500

    Add tree post order traversal

--- DataStructuresAndAlgorithms/LastAlgo/kata-machine/src/day1/BTPostOrder.ts
+++ DataStructuresAndAlgorithms/LastAlgo/kata-machine/src/day1/BTPostOrder.ts
@@ -1,3 +1,18 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function traverse(curr: BinaryNode<number> | null, path: number[]): number[] {
+  if(!curr) {
+    return path;
+  }
 
+  // recurse left
+  traverse(curr.left, path);
+  // recurse right
+  traverse(curr.right, path);
+  // visit node
+  path.push(curr.value);
+
+  return path;
+}
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+  return traverse(head, []);
 }
