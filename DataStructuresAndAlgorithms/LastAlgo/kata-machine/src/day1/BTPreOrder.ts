8bf58b613d3a6168776ad8f029602a5f33d16fdb
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2022-11-21 22:20:14 -0500

    Add pre order tree traversal

--- DataStructuresAndAlgorithms/LastAlgo/kata-machine/src/day1/BTPreOrder.ts
+++ DataStructuresAndAlgorithms/LastAlgo/kata-machine/src/day1/BTPreOrder.ts
@@ -1,3 +1,18 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function traverse(curr: BinaryNode<number> | null, path: number[]) {
+  // base case
+  if (!curr) {
+    return path;
+  }
 
+  // pre recurse - visit node
+  path.push(curr.value);
+  // recurse
+  traverse(curr.left, path); // recurse left
+  traverse(curr.right, path); // recurse right
+  // post - nothing to do in post
+  return path;
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+  return traverse(head, []);
 }
