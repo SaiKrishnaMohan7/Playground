368c5ec1f2c8bf06fc5b687b7c9e7ac4a140995e
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2022-11-21 22:26:35 -0500

    Add In order traversal

--- DataStructuresAndAlgorithms/LastAlgo/kata-machine/src/day1/BTInOrder.ts
+++ DataStructuresAndAlgorithms/LastAlgo/kata-machine/src/day1/BTInOrder.ts
@@ -1,3 +1,18 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+function traverse(curr: BinaryNode<number> | null, path: number[]): number[] {
+  if (!curr) {
+    return path;
+  }
 
+  // recurse left
+  traverse(curr.left, path);
+  // visit node
+  path.push(curr.value);
+  // recurse right
+  traverse(curr.right, path);
+  // post recurse
+  return path;
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+  return traverse(head, []);
 }
