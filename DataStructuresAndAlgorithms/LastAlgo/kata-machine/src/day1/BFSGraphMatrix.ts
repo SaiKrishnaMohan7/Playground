7bb6646ba163a597260a3254376956afc52faf7c
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2022-11-23 15:22:21 -0500

    Add BFS on graph, this was hard, need to revisit

--- DataStructuresAndAlgorithms/LastAlgo/kata-machine/src/day1/BFSGraphMatrix.ts
+++ DataStructuresAndAlgorithms/LastAlgo/kata-machine/src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,51 @@
 export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+  const seen = new Array(graph.length).fill(false);
+  const prev = new Array(graph.length).fill(-1);
 
+  seen[source] = true;
+  const q: number[] = [source];
+
+  do {
+    const curr = q.shift() as number;
+    if (curr === needle) {
+      break;
+    }
+
+    const adjs = graph[curr];
+
+    for (let i = 0; i < graph.length; ++i) {
+      if (adjs[i] === 0) {
+        continue;
+      }
+
+      if (seen[i]) {
+        continue;
+      }
+
+      seen[i] = true;
+      prev[i] = curr;
+      q.push(i);
+    }
+    seen[curr] = true;
+  } while (q.length);
+
+  if (prev[needle] === -1) {
+    return null;
+  }
+
+  // walk back and build it backwards
+
+  let curr = needle;
+  const out: number[] = [];
+
+  while (prev[curr] !== -1) {
+    out.push(curr);
+    curr = prev[curr];
+  }
+
+  if (out.length) {
+    return [source].concat(out.reverse());
+  }
+
+  return [];
 }
