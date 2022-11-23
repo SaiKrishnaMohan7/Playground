f98485e41cc53f0850b5621be8656f442c4d0ce1
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2022-11-23 12:08:03 -0500

    Add DFS on bst

--- DataStructuresAndAlgorithms/LastAlgo/kata-machine/src/day1/DFSOnBST.ts
+++ DataStructuresAndAlgorithms/LastAlgo/kata-machine/src/day1/DFSOnBST.ts
@@ -1,3 +1,21 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function search(curr: BinaryNode<number> | null, needle: number): boolean {
+  if (!curr) {
+    return false;
+  }
 
+  if (curr.value === needle) {
+    return true;
+  }
+
+  if (curr.value > needle) {
+    return search(curr.left, needle);
+  }
+
+  return search(curr.right, needle);
+
+}
+
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+  return search(head, needle);
 }
