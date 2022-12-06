4c8911916f487e5931597a9bfe70fa46e0e2c16a
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2022-12-05 22:11:03 -0500

    add leet 297 serialize and deserialize a binary tree
    - Solution doesn't work yet

---
+++ DataStructuresAndAlgorithms/NeetCode/Trees/serializeDeserializeBinaryTree.ts
@@ -0,0 +1,49 @@
+// 297. https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
+
+function dfsSerial (root: TreeNode | null, res: string[]): string {
+  // DFS pre order traversal
+  // Base Case
+  if (!root) {
+      return 'null';
+  }
+
+  // pre recursive
+  res.push(`${root}`);
+  // recursive case
+  dfsSerial(root.left, res);
+  dfsSerial(root.right, res);
+
+  // post recursive case
+  return res.join(',');
+}
+
+/*
+* Encodes a tree to a single string.
+*/
+function serialize(root: TreeNode | null): string {
+return dfsSerial(root, []);
+};
+
+function dfsRecon (nodes: string[], idx: number): TreeNode | null {
+  if (!nodes.length) {
+      return null;
+  }
+  if (nodes[idx] === 'null') {
+      ++idx;
+      return null;
+  }
+
+  let node = new TreeNode(+nodes[idx]);
+  idx = idx + 1;
+  node.left = dfsRecon(nodes.slice(idx), idx);
+  node.right = dfsRecon(nodes.slice(idx), idx);
+
+  return node;
+}
+/*
+* Decodes your encoded data to tree.
+*/
+function deserialize(data: string): TreeNode | null {
+  let nodes = data.split(',');
+  return dfsRecon(nodes, 0);
+};
