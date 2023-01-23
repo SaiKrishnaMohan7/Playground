ce59c0bd5582d5f88ba93cdf779a206f122a35d6
Sai Krishna Mohan <m.saikrishna.msk@gmail.com>
2023-01-23 11:26:18 -0500

    Add a faster approach to topKFreqElms solution

--- DataStructuresAndAlgorithms/NeetCode/ArraysAndHashing/top-k-frequent-elements.js
+++ DataStructuresAndAlgorithms/NeetCode/ArraysAndHashing/top-k-frequent-elements.js
@@ -7,37 +7,50 @@
  * IDEA:
  *  Use array indeces as count and store the numbers from nums that match the count under
  *  those indeces in an array. Bucket sort concept used.
+ * ALGO IMPL:
+ *  Create map to count numbers; array elem as key and count as value
+ *  Create the buckets with length 1 greater than input arr length, as we are using indices
+ *    to keep track of count and if nums is length 6, it is possible that an element may repeat 6 times
+ *    and if we created bucket of the same length as input array (0 to 5 indices) then we won't be able to
+ *    count the element repeating 6 times!
+ *  Populate the map by counting the numbers; { num: count }
+ *  Since we want K MOST FREQUENT, we start looping from behind
  *
  * Time and Space O(n)
  */
 var topKFrequent = function(nums, k) {
-  // Create map to count numbers; array elem as key and count as value
-  const frequencyMap = {};
-  // Create the buckets with length 1 greater than input arr length, as we are using indices
-  // to keep track of count and if nums is length 6, it is possible that an element may repeat 6 times
-  // and if we created bucket of the same length as input array (0 to 5 indices) then we won't be able to
-  // count the element repeating 6 times!
-  const bucket = Array.from({ length: nums.length + 1 }, () => []); // [[], [], [],...,[]]
+  const freqMap = new Map();
+  const buckets = Array.from({ length: nums.length + 1 }, () => []);
   const result = [];
 
-  // Populate the map by counting the numbers; { num: count }
-  for (let num of nums) {
-    frequencyMap[num] = frequencyMap[num] ? frequencyMap[num] + 1 : 1;
+  for(let i = 0; i < nums.length; ++i) {
+    let num = nums[i];
+    if (freqMap.has(num)) {
+        let count = freqMap.get(num);
+        freqMap.set(num, ++count);
+    } else {
+        freqMap.set(num, 1);
+    }
   }
 
-  // Fill the buckets with elements that match the count (indices are count)
-  for (let num in frequencyMap) {
-    bucket[frequencyMap[num]].push(num);
+  for([num, count] of freqMap) {
+    const bucket = buckets[count];
+
+    bucket.push(num);
   }
 
-  // Since we want K MOST FREQUENT, we start looping from behind
-  for (let i = bucket.length - 1; i >= 0; i--) {
-    if (bucket[i].length > 0) {
-      bucket[i].forEach(num => result.push(num));
+  for(let i = buckets.length - 1; i >= 0; --i) {
+    const bucket = buckets[i];
+
+    if (bucket.length > 0) {
+      bucket.forEach(num => result.push(num));
 
       if (result.length == k) {
           return result;
       }
     }
   }
+
+  return result;
 };
+console.log(topKFrequent([1,1,1,2,2,3], 2));
