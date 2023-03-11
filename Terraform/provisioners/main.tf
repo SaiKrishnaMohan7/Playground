f28b4945cc2dee9a5f88a98863cf4030c1ad2fb2
Sai Krishna Mohan <m.saikrishna.msk@gmail.com>
2023-03-11 12:58:26 -0500

    Add code on provisioners

---
+++ Terraform/provisioners/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "webserver" {
+  ami                         = data.aws_ssm_parameter.webserver-ami.value
+  instance_type               = "t3.micro"
+  key_name                    = aws_key_pair.webserver-key.key_name
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.sg.id]
+  subnet_id                   = aws_subnet.subnet.id
+  provisioner "remote-exec" { # IIRC we can also point to shell scripts using the path fn
+    inline = [
+      "sudo yum -y install httpd && sudo systemctl start httpd",
+      "echo '<h1><center>My Test Website With Help From Terraform Provisioner</center></h1>' > index.html",
+      "sudo mv index.html /var/www/html/"
+    ]
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("~/.ssh/id_rsa")
+      host        = self.public_ip
+    }
+  }
+  tags = {
+    Name = "webserver"
+  }
+}
