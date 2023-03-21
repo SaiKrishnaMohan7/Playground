0cb7c13c376f4e84f73dc3ae1f498ef8a72fc060
Sai Mohan <sai.mohan@aquanow.io>
2023-03-21 09:26:30 -0400

    Add terraform state file inspect samples

---
+++ Terraform/terraform_state_file/main.tf
@@ -0,0 +1,38 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "kubernetes_deployment" "tf-k8s-deployment" {
+  metadata {
+    name = "tf-k8s-deploy"
+    labels = {
+      name = "terraform-k8s-deployment"
+    }
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        name = "terraform-k8s-deployment"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          name = "terraform-k8s-deployment"
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx"
+          name  = "nginx"
+
+        }
+      }
+    }
+  }
+}
