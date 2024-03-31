c99a113455e87ef78d08fdba2350068f9616f59f
Sai Mohan <sai.mohan@labatt.com>
2024-03-31 14:00:20 -0400

    feat: add coffee entity and update Coffee Module
    - Coffee entity is added
    - Coffee Module is updated to include Coffee
    entity

--- JS/NestJs/i-luv-coffee/src/coffees/coffees.module.ts
+++ JS/NestJs/i-luv-coffee/src/coffees/coffees.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { Coffee } from './entities/coffee.entity';
 
 // Modules are way to organize code in a Nest application by business domain
 @Module({
-  imports: [], // Other modules this module imports also importing the providers those modules export
+  imports: [TypeOrmModule.forFeature([Coffee])], // Other modules this module imports also importing the providers those modules export
   controllers: [CoffeesController], // Routes this module exposes
   providers: [CoffeesService], // Any providers that need to be instantiated by the Nest Injector; available only in the scope of this module
   exports: [], // Providers that will be available wherever this module is imported
--- JS/NestJs/i-luv-coffee/src/coffees/entities/coffee.entity.ts
+++ JS/NestJs/i-luv-coffee/src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+// default, the names of the table are based on the lowercase name of the class; string could be passed to override the default table name
+// The module this entity is part of is importing the TypeOrmModule and using the `forFeature()` method to module aware of the entity
+@Entity()
 export class Coffee {
+  @Column()
   brand: string;
+
+  @Column()
   name: string;
+
+  @PrimaryGeneratedColumn()
   id: string;
+
+  @Column('json', { nullable: true }) // JSON type, optional field
   flavors: string[];
 }
+
+/**
+ * Entities in TS are classes that represent database tables.
+ * This is indicated by the `@Entity()` decorator.
+ * `synchronize: true` in AppModule in the TypeORM module setup tells TypeORM to automatically create database tables based on the entities + metadata defined
+ * in the class definition
+ */
