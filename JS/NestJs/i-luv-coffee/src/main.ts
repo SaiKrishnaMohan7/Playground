2e22ef196ffd54b6a06beec7d5dd0dc6ab92844d
Sai Mohan <sai.mohan@labatt.com>
2024-04-07 20:08:04 -0400

    feat: Handling exceptions with Filters
    - Nest has this idea of Filters that can be used to handle exceptions occuring in the app.
    This way we can control how the exception is handled

--- JS/NestJs/i-luv-coffee/src/main.ts
+++ JS/NestJs/i-luv-coffee/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,7 @@ async function bootstrap() {
       },
     }),
   );
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
