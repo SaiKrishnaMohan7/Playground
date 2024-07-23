1ae21fe830fde22d21e3e0acc74e41a95149700c
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2024-07-22 20:02:50 -0400

    finish rustlings: if else

--- Rust/rustlings/exercises/03_if/if1.rs
+++ Rust/rustlings/exercises/03_if/if1.rs
@@ -1,9 +1,10 @@
 fn bigger(a: i32, b: i32) -> i32 {
-    // TODO: Complete this function to return the bigger number!
     // If both numbers are equal, any of them can be returned.
-    // Do not use:
-    // - another function call
-    // - additional variables
+    if a < b {
+        b
+    } else {
+        a
+    }
 }
 
 fn main() {
