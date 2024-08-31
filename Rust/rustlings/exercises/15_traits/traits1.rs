d0d4d226f529b027930c52b6c28f2fa6a1c6e623
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2024-08-31 14:45:27 -0400

    Wrap up Traits
    - Traits in Rust are like interfaces in other languages

--- Rust/rustlings/exercises/15_traits/traits1.rs
+++ Rust/rustlings/exercises/15_traits/traits1.rs
@@ -5,7 +5,11 @@ trait AppendBar {
 }
 
 impl AppendBar for String {
-    // TODO: Implement `AppendBar` for the type `String`.
+    fn append_bar(mut self) -> Self {
+        self.push_str("Bar");
+
+        self
+    }
 }
 
 fn main() {
--- Rust/rustlings/exercises/15_traits/traits4.rs
+++ Rust/rustlings/exercises/15_traits/traits4.rs
@@ -10,8 +10,8 @@ struct OtherSoftware;
 impl Licensed for SomeSoftware {}
 impl Licensed for OtherSoftware {}
 
-// TODO: Fix the compiler error by only changing the signature of this function.
-fn compare_license_types(software1: ???, software2: ???) -> bool {
+// This fn will work for any type that implements the Licensed trait
+fn compare_license_types(software1: impl Licensed, software2: impl Licensed) -> bool {
     software1.licensing_info() == software2.licensing_info()
 }
 
